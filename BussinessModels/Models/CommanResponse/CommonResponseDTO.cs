50ae4bd8134a299e55f5ef64cefc3038d534b982
vishalsmartdataSDD <143817030+vishalsmartdataSDD@users.noreply.github.com>
2024-04-11 02:08:14 +0530

    Add files via upload

---
+++ BussinessModels/Models/CommanResponse/CommonResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace BussinessModels.Models.CommanResponse
+{
+
+    public class CommonResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public CommonResponse(bool success, string message, T data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+    }
+}
